fbe64e60cc15d69d4bbd784dd891c7ee2897cbe8
Wes <wfa207@gmail.com>
2016-05-11 17:04:58 -0400

    commit 5:04 pm

--- public/js/jQuerystuffs.js
+++ public/js/jQuerystuffs.js
@@ -9,22 +9,22 @@ for (var key in activities) {
 }
 
 $('.btn-hotel').click(function() {
-  var selectedHotel = $('#hotel-choices option:selected').text();
+  var selectedHotel = $('#hotel-choices option:selected');
   console.log(selectedHotel);
-  $('.itinerary-item.hotel-item').append('<span class="title">'+selectedHotel+"</span>");
+  $('.itinerary-item.hotel-item').append('<span class="title">'+selectedHotel.text()+"</span>");
   $('.itinerary-item.hotel-item').append('<button class="btn btn-xs btn-danger remove btn-circle">x</span>');
 });
 
 $('.btn-restaurant').click(function() {
-  var selectedRestaurant = $('#restaurant-choices option:selected').text();
+  var selectedRestaurant = $('#restaurant-choices option:selected');
   console.log(selectedRestaurant);
-  $('.itinerary-item.restaurant-item').append('<span class="title">'+selectedRestaurant+"</span>");
+  $('.itinerary-item.restaurant-item').append('<span class="title">'+selectedRestaurant.text()+"</span>");
   $('.itinerary-item.restaurant-item').append('<button class="btn btn-xs btn-danger remove btn-circle">x</span>');
 });
 
 $('.btn-activity').click(function() {
-  var selectedActivity = $('#activity-choices option:selected').text();
+  var selectedActivity = $('#activity-choices option:selected');
   console.log(selectedActivity);
-  $('.itinerary-item.activity-item').append('<span class="title">'+selectedActivity+"</span>");
+  $('.itinerary-item.activity-item').append('<span class="title">'+selectedActivity.text()+"</span>");
   $('.itinerary-item.activity-item').append('<button class="btn btn-xs btn-danger remove btn-circle">x</span>');
 });
