54f65a63797a3db3722267147d19ee145cb7612e
Wes <wfa207@gmail.com>
2016-05-11 16:16:35 -0400

    drop downs fixed

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var place = require('../models/place');
+var activity = require('../models/activity');
+var restaurant = require('../models/restaurant');
+var hotel = require('../models/hotel');
 
-router.get('/', function(req, res) {
-  res.render('index');
+router.get('/', function(req, res, next) {
+  Promise.all([hotel.findAll({}), restaurant.findAll({}), activity.findAll({})])
+    .then(function(results) {
+        // var hotel = results[0];
+        // var restaurant = results[1];
+        // var activity = results[2];
+    	res.render('index', {
+    		hotels: results[0],
+    		restaurants: results[1],
+    		activities: results[2]
+    	});
+	})
 });
 
 module.exports = router;
